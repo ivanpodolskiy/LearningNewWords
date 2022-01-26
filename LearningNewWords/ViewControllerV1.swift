afbdff59c5411533a2f79f286887bb3859a8b4b2
ivanpodolskiy <stren.25@mail.ru>
2022-01-26 19:47:29 +0300

    dev 0.1.1
    add soundTest

--- LearningNewWords/ViewControllerV1.swift
+++ LearningNewWords/ViewControllerV1.swift
@@ -6,42 +6,48 @@
 //
 
 import UIKit
+import AVFoundation
+import AudioToolbox
 
 class ViewControllerV1: UIViewController {
     
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
     }
     
+    
+    //Словарь в котором находятся слова и перевод.
+    var  dictionary: [String:String] = ["Кот":"Cat", "Собака":"Dog","Рыба":"Fish"]
+    
+    //Переменные для временного хранения значения словоря
+    var checkValue: String = ""
+    var checkValueSecond: String = ""
+    
+    
+    //Индекаторы
     @IBOutlet weak var indicatorLabel: UILabel!
     @IBOutlet weak var numIndicator: UILabel!
-
     @IBOutlet weak var maxNumIndicator: UILabel!
+    
     
     @IBAction func printButton(_ sender: Any) {
         statusLable.text = ("dictionary = \(dictionary); ValueNow: \(checkValueSecond) or \(checkValue)")
     }
-    
     @IBOutlet weak var statusLable: UILabel!
-    //Словарь в котором находятся слова и перевод.
-    var  dictionary: [String:String] = ["Кот":"Cat", "Собака":"Dog","Рыба":"Fish"]
     
-    
-    var checkValue: String = ""
-    var checkValueSecond: String = ""
     
     //Кнопка которая добавит повторно содержимое в словарь
     @IBAction func addButton(_ sender: UIButton) {
         dictionary = ["Кот":"Cat", "Собака":"Dog","Рыба":"Fish"]
         print (dictionary)
         print (checkValueSecond)
+        
     }
     
     //Функция для проверки ответа
-    func testCheck(One: Dictionary<String, String>){
+    func start(One: Dictionary<String, String>){
+        
         if dictionary == [:] {
             resultLabel.text = "Нет слов для проверки"
             wordLabel.text = ""
@@ -63,20 +69,19 @@ class ViewControllerV1: UIViewController {
     }
     
     @IBOutlet weak var wordLabel: UILabel!
-    
-    
     @IBOutlet weak var textField: UITextField!
     
     //Кнопка для старта
     @IBAction func startButton(_ sender: UIButton) {
+        AudioServicesPlaySystemSound(SystemSoundID(1104))
         if dictionary != [:]{
             maxNumIndicator.text = String(dictionary.count)
             numIndicator.text = "1"
-
+            
         }
- 
-        testCheck(One: dictionary)
-       
+        
+        start(One: dictionary)
+        
     }
     
     //Алерт сообщение
@@ -88,9 +93,10 @@ class ViewControllerV1: UIViewController {
         message.addAction(ok)
         self.present(message, animated: .random(), completion: nil)
     }
-    @IBAction func checkButton(_ sender: Any) {
-       
-        
+    
+    
+    //Функция для проверки ответов
+    func check() {
         if dictionary == [:] || textField.text == "" {
             wordLabel.text = "No"
             resultLabel.text = "Слов больше нет"
@@ -113,8 +119,6 @@ class ViewControllerV1: UIViewController {
                 let randomKay = Array(dictionary.keys)[index]
                 let randomVal = Array(dictionary.values)[index]
                 dictionary[randomKay] = nil
-                
-               
                 wordLabel.text = randomKay
                 checkValueSecond = randomVal
                 let test = indicatorLabel.text
@@ -123,34 +127,52 @@ class ViewControllerV1: UIViewController {
                 numIndicator.text = String(num)
                 
             }
-           
+            
         } else {
             resultLabel.text = "Не верно"
             resultLabel.textColor = .red
-        checkValue = ""
-       checkValueSecond  = ""
+            checkValue = ""
+            checkValueSecond  = ""
             let index: Int = Int(arc4random_uniform(UInt32(dictionary.count)))
             let randomKay = Array(dictionary.keys)[index]
             let randomVal = Array(dictionary.values)[index]
             dictionary[randomKay] = nil
-            
             wordLabel.text = randomKay
             checkValueSecond = randomVal
-            
             let test = indicatorLabel.text
             indicatorLabel.text = "\(test ?? "")🔴"
             let num: Int = Int(numIndicator.text!)! + 1
             numIndicator.text = String(num)
         }
-   
         
-    
         
     }
     
-
+    @IBAction func checkButton(_ sender: Any) {
+        AudioServicesPlaySystemSound(SystemSoundID(1104))
+        check()
+    }
+    
+    
     
     @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    //Add new
+    @IBOutlet weak var addWordFieldFirst: UITextField!
+    
+    @IBOutlet weak var addWordFieldSecond: UITextField!
+    
+    @IBAction func addNewWordsButton(_ sender: UIButton) {
+        let firstWord = String(self.addWordFieldFirst.text!)
+        let secondWord = String(self.addWordFieldSecond.text!)
+        self.dictionary[firstWord] = secondWord
+        //        self.array.append(firstWord)
+        //        self.wordLabel.text = addWordFieldFirst.text
+        print(dictionary)
+        self.addWordFieldFirst.text = ""
+        self.addWordFieldSecond.text = ""
+    }
 }
 
 
